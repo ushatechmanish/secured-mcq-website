b0686370b8c8c8b56eea7e75803fd40f2675ef5c
Manish Chauhan <manish.chauhan@ushatech.in>
2023-07-09 18:17:17 +0530

    Disable security for all. closes #2

--- src/main/java/in/ushatech/securedmcqwebsite/security/SecurityConfigurationAdaptor.java
+++ src/main/java/in/ushatech/securedmcqwebsite/security/SecurityConfigurationAdaptor.java
@@ -2,6 +2,7 @@ package in.ushatech.securedmcqwebsite.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 @Configuration
@@ -14,5 +15,10 @@ public class SecurityConfigurationAdaptor extends WebSecurityConfigurerAdapter
                 .withUser("user")
                 .password("{noop}pa55w0rd")
                 .roles("USER");
+    }
+
+    protected void configure(HttpSecurity http) throws Exception
+    {
+        http.authorizeRequests().antMatchers("*/**").permitAll();
     }
 }
