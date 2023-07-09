3514b01b5104a75111b6b2e762ec671150bfe1bc
Manish Chauhan <135151930+ushatechmanish@users.noreply.github.com>
2023-07-09 15:43:58 +0530

    Initial commit

---
+++ src/main/java/in/ushatech/securedmcqwebsite/SecuredMcqWebsiteApplication.java
@@ -0,0 +1,13 @@
+package in.ushatech.securedmcqwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuredMcqWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecuredMcqWebsiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/in/ushatech/securedmcqwebsite/SecuredMcqWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package in.ushatech.securedmcqwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuredMcqWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
